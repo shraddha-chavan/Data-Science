524b911400223ed23d3477253e35d90d5a91a59e
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-03-10 12:10:51 +0800

    Add files via upload

---
+++ bubble_sort .py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 10 09:12:22 2025
+
+@author: Latitude 5410
+"""
+
+def bubble_sort(lst):
+    n=len(lst)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if lst[j]>lst[j+1]:
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+    return lst
+lst=[2,4,5,6,3]
+print(bubble_sort(lst))
+ 
+           
+    
+    
