20cf91dc25b12bd52a6272c47435ec1aa1bef59e
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-03-11 11:57:34 +0800

    Update Gcd.py

--- 1-python/Gcd.py
+++ 1-python/Gcd.py
@@ -27,6 +27,26 @@ print(f"GCD of {num1} and {num2} is :{ans} ")
 
 
 
+#LCM of two numbers
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+def gcd(num1,num2):
+    while num2!=0:
+        num1,num2=num2,num1%num2
+    return num1
+gcd(num1,num2)
+
+def lcm(num1,num2):
+    lcm=abs(num1*num2/gcd(num1,num2))
+    return lcm
+lcm(num1,num2)    
+
+    
+    
+
+
+
+
 
 
 
