cfe1341a6df3f7dc334fbcd0b64adc282b5a267a
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-03-11 11:14:13 +0800

    Update bubble_sort .py

--- 1-python/bubble_sort .py
+++ 1-python/bubble_sort .py
@@ -26,6 +26,20 @@ def bubble_sort(lst):
 lst=[2,4,5,6,3]
 print(bubble_sort(lst))
 print(lst[-2])
+
+#finding second smallest element in the sorted array
+def bubble_sort(lst):
+    n=len(lst)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if lst[j]>lst[j+1]:
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+    return lst
+lst=[2,4,5,6,3]
+print(bubble_sort(lst))
+print(lst[-4])
+
+
  
            
     
