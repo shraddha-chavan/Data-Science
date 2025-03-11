adca09ea51ce6e2924d6244e398e30490898125a
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-03-11 11:34:27 +0800

    Update bubble_sort .py

--- 1-python/bubble_sort .py
+++ 1-python/bubble_sort .py
@@ -39,6 +39,47 @@ lst=[2,4,5,6,3]
 print(second_smallest_element(lst))
 print(lst[1])
 
+#finding second largest number in an array for duplicate values
+#for smallest print unique_nums[-2]
+def second_largest_element(lst):
+    unique_nums=list(set(lst))#list is converted in to set
+    unique_nums.sort(reverse=True)#it is comverted back into a list
+    # the sort (reverse = true)method in python sorts
+    #a list in desending order(from highest to lowest)
+    if len(unique_nums)>1:
+        
+        return unique_nums[1]
+    else:
+        return None
+lst=[10,20,40,53,99]
+print("Second largest:", second_largest_element(lst))    
+
+
+#for smallest print unique_nums[-2]
+#finding second smallest element in an sorted array
+def second_smallest_element(lst):
+    unique_nums=list(set(lst))#list is converted in to set
+    unique_nums.sort(reverse=True)#it is comverted back into a list
+    # the sort (reverse = true)method in python sorts
+    #a list in desending order(from highest to lowest)
+    if len(unique_nums)>1:
+        
+        return unique_nums[-2]
+    else:
+        return None
+lst=[10,20,40,53,99]
+print("Second smallest:", second_smallest_element(lst))    
+
+
+
+
+
+
+
+
+
+
+
 
  
            
