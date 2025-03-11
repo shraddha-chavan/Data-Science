3aad2ab70cfdb2afdd1de612dc50d5b022787468
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-03-11 11:06:11 +0800

    Update bubble_sort .py

--- 1-python/bubble_sort .py
+++ 1-python/bubble_sort .py
@@ -14,6 +14,18 @@ def bubble_sort(lst):
     return lst
 lst=[2,4,5,6,3]
 print(bubble_sort(lst))
+
+#finding second largest element in the sorted array
+def bubble_sort(lst):
+    n=len(lst)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if lst[j]>lst[j+1]:
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+    return lst
+lst=[2,4,5,6,3]
+print(bubble_sort(lst))
+print(lst[-2])
  
            
     
