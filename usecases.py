0200ee9c25bdcf905034e8b388cb105e6d6f32c2
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-02 12:06:10 +0800

    Update usecases.py

--- usecases.py
+++ usecases.py
@@ -675,6 +675,78 @@ def say_hi():
 say_hi()
 
 
+###split string and uppercase#################
+def split_string(function):
+    def wrapper():
+        func = function()
+        splitted_string = func.split()
+        return splitted_string
+    return wrapper
+def uppercases_decorator(function):
+    def wrapper():
+        func=function()
+        make_uppercase=func.upper()
+        return make_uppercase
+    return wrapper
+
+
+@split_string
+@uppercase_decorator
+def say_hi():
+    return'hello there'
+
+say_hi()
+
+#############decorator for squares and cubes#############
+import time
+def time_it(func):
+    
+    #this is a decorator function 
+    #that take another functiom
+    def wrapper(*args,**kwargs):
+        #*args and ** kwargs allows wrapper
+        #to accept any number of 
+        #positional and keyword
+        start=time.time()
+        result=func(*args,**kwargs)
+        #calls the original function(func)
+        #with the provided arguements
+        end=time.time()
+        print(func.__name__+'took'+str((end-start)*1000)+ ' mili second')
+        return result
+    return wrapper
+
+@time_it
+def calc_square(numbers):
+    result=[]
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+
+@time_it
+def calc_cube(numbers):
+    result=[]
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
+
+array = range(1,100000)
+out_square= calc_square(array)
+out_square= calc_cube(array)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
