3e44200d8fe61801d934570db306362bfabcdf08
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-03 11:40:03 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -28,3 +28,47 @@ for num in range_even(6):
 gen=range_even(6)
 next(gen)
 next(gen)
+
+####usecase: let us hide passward entered on screen
+###chaining generator
+def lengths(itr):
+    for ele in itr:
+        yield len(ele)
+        
+def hide(itr):
+    for ele in itr:
+        yield ele*'*'
+
+passwards=["not-good","give'm-pass","00100=100"]
+for passward in hide(lengths(passwards)):
+    print(passward)
+    
+    
+    
+    
+#############example1#####################
+words = ["apple", "banana", "kiwi"]
+for length in lengths(words):
+    print(length)
+
+##########################################
+#enumerate
+#printing lis with index
+lst=["milk","Egg","Bread"]
+for index in range(len(lst)):
+    print(f'{index+1}{lst[index]}')
+## same code using enumerate
+lst=["milk","Egg","Bread"]
+for index,item in enumerate(lst,start=1):
+    print(f'{index}{item}')
+
+
+
+
+
+
+
+
+
+
+
