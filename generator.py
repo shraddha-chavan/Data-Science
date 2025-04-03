6c27c666defef12971a2f1d1c2e9248ea0fd3474
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-03 11:39:10 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -16,4 +16,11 @@ gen=(x for x in range(3))
 next(gen)
 next(gen)
 
+########################
+#function which returns multiple values#
+def range_even(end):
+    for num in range(0,end,2):
+        yield num
+for num in range_even(6):
+    print(num)
 
