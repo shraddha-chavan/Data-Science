a8942205bbe0537fc3ab4ff62bf9bbe27ba92df3
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-03 11:38:44 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,19 @@
+####generator#####
+###implimented during functions
+###another way to cerate iterator
+###uses keyword "yeild"
+gen=(x for x in range (3))
+print(gen)
+for num in gen:
+    print(num)
+##################
+
+gen=(x for x in range(3))
+next(gen)
+
+########################
+gen=(x for x in range(3))
+next(gen)
+next(gen)
+
 
