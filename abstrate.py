42ff2446aeb236ddcb459d86549cce2e16f56f3e
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-07 18:25:55 +0800

    Update abstrate.py

--- abstrate.py
+++ abstrate.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  7 15:44:30 2025
 
+@author: Latitude 5410
+"""
+
+from abc import ABC,abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius=radius
+        
+    def area(self):
+        return 3.14*self.radius*self.radius
+##usage example
+Circle1=Circle(5)
+print("Area of circle:",Circle1.area())    
