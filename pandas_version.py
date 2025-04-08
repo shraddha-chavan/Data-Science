191f84dbeff40b8711a91242cf3819195d95b596
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-08 12:13:55 +0800

    Update pandas_version.py

--- pandas_version.py
+++ pandas_version.py
@@ -4,4 +4,9 @@ technologies=[["Spark",20000,"30days"],["pandas",20000,"40days"],]
 df=pd.DataFrame(technologies)
 print(df)
 
+#add column & rows label to the dataframe
+column_names=["Courses","Fee","Duration"]
+row_label=["a","b"]
+df=pd.DataFrame(technologies,columns=column_names,index=row_label)
+print(df)
 
