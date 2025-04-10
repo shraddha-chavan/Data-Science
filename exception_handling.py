f8e6928b73dd590e48a77c12a0e3f85dce711c56
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-10 12:20:08 +0800

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -1,1 +1,10 @@
+# Example 1: File Handling (Read CSV)
+
+import pandas as pd
+
+try:
+    df = pd.read_csv("data.csv")
+    print("File loaded successfully.")
+except FileNotFoundError:
+    print("Error: The file was not found.")
 
