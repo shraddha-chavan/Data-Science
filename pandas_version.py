124f765275e5538a2e40d88504172ed13a6f377d
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-10 18:01:43 +0800

    Update pandas_version.py

--- pandas_version.py
+++ pandas_version.py
@@ -339,7 +339,21 @@ print(df1)
 df1=df.sample(frac=1).reset_index(drop=True)
 print(df1)
 
+####################################################################
+import pandas as pd
+technologies={'Courses':["Spark","PySpark","Python","pandas"],
+              'Fee':[20000,25000,22000,30000],
+              'Duration':['30days','40days','35days','50days'],
+              }
+index_labels=['r1','r2','r3','r4']
+df1=pd.DataFrame(technologies,index=index_labels)
 
+technologies2={
+    'Courses':["Spark","Java","Python","Go"],
+    'Discount':[2000,2300,1200,2000]}
+
+index_labels2=['r1','r6','r3','r5']
+df2=pd.DataFrame(technologies2,index=index_labels2)
 
 
 
