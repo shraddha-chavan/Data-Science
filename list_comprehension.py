11772bdec1cd646856a75314f6b5a5e9226b99c1
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-12 15:17:45 +0800

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -30,7 +30,10 @@ squares = [x**2 for x in range(5)]
 print(squares)
 # Output: [0, 1, 4, 9, 16]
 
-
+#even numbers:
+evens = [x for x in range(10) if x % 2 == 0]
+print(evens)
+# Output: [0, 2, 4, 6, 8]
 
 
 
