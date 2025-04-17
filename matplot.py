134a494f2829f9a2a90e5cda682fc50cf3cc80a4
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-17 19:37:34 +0800

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -495,6 +495,36 @@ plt.yticks(x_pos,x)
 plt.show()
 #########################################################################
 
+#write a python code to craete bar plot of scores by group
+# and gender use multiple 
+import numpy as np
+import matplotlib.pyplot as plt
+#data to plot
+n_groups=5
+men_means=(22,30,33,30,26)
+women_means=(25,32,30,35,29)
+#create a plot
+fig,ax=plt.subplots()
+index=np.arange(n_groups)
+bar_width=0.35
+opacity=0.8
+rects1=plt.bar(index,men_means,bar_width,alpha=opacity,color='g',label='Men')
+rects2=plt.bar(index+bar_width,women_means,bar_width,alpha=opacity,color='r',label='Women')
+
+plt.xlabel('Person')
+plt.ylabel('Scores')
+plt.xlabel('Scores by Person')
+plt.xticks(index + bar_width,('G1','G2','G3','G4','G5'))
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
 
 
 
