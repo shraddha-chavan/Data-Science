1fd7e0b81a118655bc57a2ce8dcea473c90d4328
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-20 15:48:07 +0800

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -7,3 +7,20 @@ file = open("example.txt", "r")
 content = file.read()
 print(content)
 file.close()
+##############################################################################
+#user login system::
+# Step 1: Take input from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Step 2: Write the data to a file
+with open("user_data.txt", "w") as file:
+    file.write(f"Name: {name}\n")
+    file.write(f"Age: {age}\n")
+
+print("Data saved successfully!\n")
+
+# Step 3: Read and display the data from the file
+with open("user_data.txt", "r") as file:
+    print("Reading data from file:")
+    print(file.read())
