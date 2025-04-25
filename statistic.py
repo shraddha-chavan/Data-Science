7557fba725b0f678600be6a4f0b0d4bcec351048
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-25 13:51:46 +0530

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -1,1 +1,18 @@
-
+import pandas as pd
+import numpy as np
+###############################
+df = pd.read_csv("C:/3-python statistic/income.csv")
+df
+###############################
+df=df.rename({"Monthly Income ($)":"income"},axis=1)
+############################
+df.income.describe()
+#########################
+df.income.quantile(0)
+##############################
+df.income.quantile(0.75)
+df
+#for inserting null value
+df['income'][3]=np.NaN
+df
+############################
