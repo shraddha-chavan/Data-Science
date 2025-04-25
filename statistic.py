dc65c6c503721433f110657145d820a3ab7c3a66
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-25 13:52:10 +0530

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -16,3 +16,31 @@ df
 df['income'][3]=np.NaN
 df
 ############################
+df.income.mean()
+###################for mean
+df_new=df.fillna(df.income.mean())
+df_new
+##################for median
+df_new=df.fillna(df.income.median())
+df_new
+############################################################################
+
+import numpy as np
+#sample dataset
+data = [10,12,23,23,16,23,21,16]
+print("Original Data:",data)
+
+
+#step1:mean of the data
+mean=np.mean(data)
+print("\nMean (Average):",mean)
+
+#step2:mean absolute deviation(MAD)
+#Its the average of absolute deviations from the mean
+mad = np.mean([abs(x-mean)for x in data])
+print("Mean Absolute Deviation(MAD):",mad)
+
+#step3:Variance
+#its the average of square differnce from the mean
+variance=np.var(data)
+print("Variance:",variance)
