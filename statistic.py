6da78e2568ce8adf8ae58e8087a79d2b09b36511
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-25 13:52:37 +0530

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -44,3 +44,47 @@ print("Mean Absolute Deviation(MAD):",mad)
 #its the average of square differnce from the mean
 variance=np.var(data)
 print("Variance:",variance)
+#step4:SD
+#its the square root of the variance
+std_dev=np.std(data)
+print("SD:",std_dev)
+
+#######################################################################
+#history and math test score from the image
+history_scores=[75,72,68,65,67,73]
+math_scores=[93,96,43,47,51,90]
+
+def calculate_mad(scores):
+    mean=sum(scores)/len(scores)
+    mad=sum(abs(x-mean)for x in scores)/len(scores)
+    return mad
+#calculate MAD
+history_mad=calculate_mad(history_scores)
+math_mad=calculate_mad(math_scores)
+
+print("Mean Absolute Deviation(MAD):")
+print(f"History test:{history_mad:.2f}")
+print(f"Math Test:{math_mad:.2f}")
+
+'''👉 Interpretation: Students' performance was 
+more consistent in History, while Math scores 
+varied widely — possibly indicating that some
+ students struggled significantly more than
+ others in Math.
+ 
+ 👉Strategic Decision:The math scores show a big 
+ difference between students, meaning some are 
+ doing very well while others are struggling. 
+ To fix this, teachers can first find out which 
+ topics are hard for students. They should give
+ extra help to those who need it and let strong 
+ students help others. Using fun methods like games,
+ pictures, or real-life examples can make learning easier. 
+ Regular small tests can help check understanding early.
+ Teachers should also talk to parents for support 
+ and encourage students to keep trying, so they 
+ feel confident in math again. The goal is to help
+ every student improve, not just raise the class average.
+
+'''
+##################################################################################
