260e16b6c7e67b78e64f207b55af21ba4ff2364b
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-04-26 13:16:37 +0530

    Update usecasaes_statistic.py

--- usecasaes_statistic.py
+++ usecasaes_statistic.py
@@ -1,1 +1,13 @@
+##############################################################################################
+#1. Heatmap of Correlation Matrix 
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris = sns.load_dataset("iris")
+corr_matrix = iris.corr()
+
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')#heatmap
+plt.title("Correlation Matrix Heatmap - Iris Dataset")
+plt.show()
 
