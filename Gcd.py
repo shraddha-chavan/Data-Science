a4350cac6815992c8dd72f3928617d59095eddd4
Shraddha Chavan <146915989+shraddha-chavan@users.noreply.github.com>
2025-05-15 19:37:42 +0530

    Update Gcd.py

--- Gcd.py
+++ Gcd.py
@@ -4,7 +4,7 @@ Created on Mon Mar 10 09:26:32 2025
 
 @author: Latitude 5410
 """
-
+####################################################
 def gcd(a,b):
     while b:
         a,b=b,a%b#replace a with b and b with remainder
